e0937453b9fa413cf2d49f4f40dce972dfd2ed95
jhon <fr.jhonk@gmail.com>
2023-09-29 12:49:46 -0500

    done basic

--- csdid/ATTgt.py
+++ csdid/ATTgt.py
@@ -1,6 +1,7 @@
 from .utils import *
 from .mboot import *
 from .utils_aggte import *
+from .plots import *
 
 
 from pyspark.sql import  SparkSession, Row
@@ -9,7 +10,10 @@ from pyspark.sql.functions import \
 	monotonically_increasing_id, desc, mean
 
 import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+
+
 
 
 from drdid import drdid, reg_did
@@ -489,6 +493,7 @@ class ATTgt:
       'att': att_est,
       'post': post_array,
       'se': se,
+      'c': crit_val,
       'upper': cband_lower,
       'lower': cband_upper,
       'sig': sig_text 
@@ -871,4 +876,111 @@ class ATTgt:
         self.summary_aggte['calendar'] = calendar_aggte
 
   
+  def plot_attgt(self, ylim=None, 
+                xlab=None, 
+                ylab=None, 
+                title="Group",
+                xgap=1, 
+                ncol=1, 
+                legend=True, 
+                group=None, 
+                ref_line=0,
+                theming=True, 
+                grtitle="Group"
+                ):
 
+    did_object = self.output
+
+    grp = did_object['group']
+    t_i = did_object['time']
+
+    G = len(np.unique(grp))
+    Y = len(np.unique(t_i))
+    g = np.unique(grp)[np.argsort(np.unique(grp))].astype(int)
+    y = np.unique(t_i)
+
+    results = pd.DataFrame({'year': np.tile(y, G)})
+    results['group'] = np.repeat(g, Y)
+    results['grtitle'] = grtitle + ' ' + results['group'].astype(str)
+    results['att'] = did_object['att']
+    results['att_se'] = did_object['se']
+    results['post'] = np.where(results['year'] >= grp, 1, 0)
+    results['year'] = results['year']
+    results['c'] = did_object['c']
+
+    self.results_plot_df_attgt = results
+
+    if group is None:
+      group = g
+      if any(group not in g for group in group):
+        raise "Some of the specified groups do not exist in the data. Reporting all available groups."
+
+
+    legend_1 = False    # for multiple subplots, legend outside 
+    fig, axes = plt.subplots(nrows=len(group), ncols=1, figsize=(10, 5))  # Adjust the figsize as needed
+    handles = []
+    labels = []
+    for i, group_cat in enumerate(group):
+        group_data = results.loc[results['group'] == group_cat]
+        title = group_data['grtitle'].unique()[0]
+        ax = axes[i]
+        ax = gplot(group_data, ax, ylim, xlab, ylab, title, xgap, legend_1, ref_line, theming)
+    plt.tight_layout()
+    if legend is True:
+        handles_ax, labels_ax = ax.get_legend_handles_labels()
+        handles.extend(handles_ax)
+        labels.extend(labels_ax)
+        fig.legend(handles, labels, loc='lower center', fontsize='small', bbox_to_anchor=(0.545, -0.075), ncol=2)
+    
+    plt.show()
+    return fig 
+  def plot_aggte(self, 
+          typec = 'group',
+          ylim=None, 
+                    xlab=None, 
+                    ylab=None, 
+                    title="", 
+                    xgap=1, 
+                    legend=True, 
+                    ref_line=0, 
+                    theming=True,
+                    **kwargs):
+
+    did_object = self.summary_aggte['group']
+
+    post_treat = 1 * (np.asarray(did_object["egt"]).astype(int) >= 0)
+    
+    results = {
+        "year": list(map(int, did_object["egt"])),
+        "att": did_object["att_egt"],
+        "att_se": did_object["se_egt"][0],
+        "post": post_treat
+    }
+    
+    results = pd.DataFrame(results)
+    self.results_plot_df_aggte = results
+    
+    if did_object['crit_val_egt'] is None:
+        results['c'] = abs(norm.ppf(0.025))
+    else:
+        results['c'] = did_object['crit_val_egt']
+
+    if title == "":
+        title = "Average Effect by Group" if\
+          did_object["type"] == "group" else\
+            "Average Effect by Length of Exposure"
+
+
+    if did_object["type"] == "group":
+        fig, ax = plt.subplots(figsize=(10, 5))
+        p = splot(results, ax, ylim, xlab, ylab, title, legend, ref_line, theming)
+        plt.tight_layout()
+        plt.show()
+
+    else:
+        fig, ax = plt.subplots(figsize=(10, 5))
+        p = gplot(results, ax, ylim, xlab, ylab, title, xgap, legend, ref_line, theming)
+        plt.tight_layout()
+        plt.show() 
+        
+    return p
